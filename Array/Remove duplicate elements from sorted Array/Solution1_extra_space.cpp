d204a2d581c225b534b4e0f6c6a7f41bdc917d03
AKASH KUMAR <akash30521@gmail.com>
2022-02-02 00:18:06 +0530

    Create Solution1_extra_space.cpp

---
+++ Array/Remove duplicate elements from sorted Array/Solution1_extra_space.cpp
@@ -0,0 +1,18 @@
+class Solution{
+public:
+    int remove_duplicate(int a[],int n){
+        // code here
+        int j=0;
+        int temp[n];
+        for(int i=0;i<n-1;i++){
+            if(a[i]!=a[i+1]){
+                temp[j++] = a[i];
+            }
+        }
+        temp[j++] = a[n-1];
+        for(int i=0;i<j;i++){
+            a[i] = temp[i];
+        }
+        return j;
+    }
+};
