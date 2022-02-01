e879b86912825db0867c98fc0cda98e337f1b128
AKASH KUMAR <akash30521@gmail.com>
2022-02-02 00:29:46 +0530

    Create solution_2_O(1)_space.cpp

---
+++ Array/Remove duplicate elements from sorted Array/solution_2_O(1)_space.cpp
@@ -0,0 +1,12 @@
+    int remove_duplicate(int a[],int n){
+        int j=0;
+        for(int i=0;i<n-1;i++){
+            if(a[i]!=a[i+1]){
+               a[j++] = a[i];
+            }
+        }
+        a[j++] = a[n-1];
+        return j;
+    }
+  // Time complexity = O(n)
+  // SC = O(1)  
