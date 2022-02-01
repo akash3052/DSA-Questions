6a78fd52652ee1e90ac3f74d57b7df7e0012e857
AKASH KUMAR <akash30521@gmail.com>
2022-02-02 01:06:44 +0530

    Update solution_1_brute_force.cpp

--- Array/Largest subarray with 0 sum/solution_1_brute_force.cpp
+++ Array/Largest subarray with 0 sum/solution_1_brute_force.cpp
@@ -1,23 +1,28 @@
 class Solution{
     public:
     int maxLen(vector<int>&A, int n){   
-       int finalLength =0;
-       // considering all subarrays and checking sum in that subarray , if that equal to 0, we are updating finalLength's value(which is basically lenght of the subarray).
+       int maxLength =0;
        for(int i=0;i<n;i++){
-           int sum = A[i];
-           // length can be 1 also, that's why considering if single element's value is 0, if it is then update finalLength to max(finalLenght,1).
-           if(sum == 0){
-               finalLength = max(finalLength,1);
-           }
-           for(int j=i+1;j<n;j++){
-               sum+=A[j];
-               if(sum ==0){
-                   finalLength = max(finalLength , j-i+1);
+           // Initialize currr_sum for every starting point
+           int currSum = 0;
+           
+           // try all subarrays starting with 'i'
+           for(int j=i;j<n;j++){
+               currSum+=A[j];
+               
+               // If curr_sum becomes 0, then update maxLength if required
+               if(currSum ==0){
+                   maxLength = max(maxLength , j-i+1);
                }
            }
        }
-       return finalLength;
+       return maxLength;
     }
 };
+
+// Algorithm:  
+// 1. Consider all sub-arrays one by one and check the sum of every sub-array.
+// 2. Run two loops: the outer loop picks the starting point i and the inner loop tries all sub-arrays starting from i.
+
 // TC = O(n^2)
 // SC = O(1)
