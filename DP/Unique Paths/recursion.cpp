146eb18fc98a97c044964f6371e790587e4412f0
AKASH KUMAR <akash30521@gmail.com>
2023-08-22 23:51:39 +0530

    Create recursion.cpp

---
+++ DP/Unique Paths/recursion.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+int solve(int i, int j, int &m, int &n, vector<vector<int>> &dp){
+	if(i == m-1 && j == n-1){
+		return 1;
+	}
+	if(i >= m || j >= n){
+		return 0;
+	}
+	if(dp[i][j] != -1){
+		return dp[i][j];
+	}
+	int right = solve(i, j+1, m, n, dp);
+	int down = solve(i+1, j, m, n, dp);
+	return dp[i][j] = right + down;
+}
+int uniquePaths(int m, int n) {
+	vector<vector<int>> dp(m, vector<int>(n,-1));
+	return solve(0, 0, m, n, dp);
+}
+
+// TC = O(2 ^ (m+n)) -> for every cell, there are 2 possibilities and there are (m+n) cells in total.
+// SC = O((m-1)+ (n-1))
+
