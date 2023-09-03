011ff057c46fb13d99982629600ae45756f79113
AKASH KUMAR <akash30521@gmail.com>
2023-09-03 14:49:09 +0530

    Create solution.cpp

---
+++ Array/Merge 2 Sorted Array/solution.cpp
@@ -0,0 +1,39 @@
+vector < int > sortedArray(vector < int > a, vector < int > b) {
+    // Write your code here
+    vector<int> unionArr;
+    int i = 0, j = 0;
+    int n1 = a.size(), n2 = b.size();
+    while(i < n1 && j < n2){
+        if(a[i] <= b[j]){
+            // unionArr.size() == 0 -> without checking this, it was giving error because we have not inserted anything in the
+            // array and we are checking its back().
+            // unionArr.back() != a[i] -> this condition is for removing duplicate elements from final result.
+            if(unionArr.size() == 0 || unionArr.back() != a[i]){
+                unionArr.push_back(a[i]);
+            }
+            i++;
+        }else{
+            if(unionArr.size() == 0 || unionArr.back() != b[j]){
+                unionArr.push_back(b[j]);
+            }
+            j++;
+        }
+    }
+    // if some elements is left in the 'a' array.
+    while(i < n1){
+        if(unionArr.size() == 0 || unionArr.back() != a[i]){
+            unionArr.push_back(a[i]);
+        }
+        i++;
+    }
+    // if some elements is left in the 'b' array.
+    while(j < n2){
+        if(unionArr.size() == 0 || unionArr.back() != b[j]){
+            unionArr.push_back(b[j]);
+        }
+        j++;
+    }
+    return unionArr;
+}
+
+
