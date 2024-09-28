089ba57c8abc1bcc7ad02bf045ae82f870465a0f
AKASH KUMAR <akash30521@gmail.com>
2024-09-28 13:34:05 +0530

    Update memoization_0_to_n-1.cpp

--- DP/Maximum sum of non-adjacent elements/memoization_0_to_n-1.cpp
+++ DP/Maximum sum of non-adjacent elements/memoization_0_to_n-1.cpp
@@ -2,6 +2,8 @@
 int solve(int index, int n, vector<int> &nums, vector<int> &dp){
     // we would have reached n-1 only from n-3, hence we are adding its value in the answer. 
     // we could not reached n-1 from n-2 because of adjacent constraints.
+    // This condition is little bit confusing. See, below solution for better understanding.
+    // Even if we remove below condition, our code will work because same thing will be done in line 17.
     if(index == n-1){
         return nums[index];
     }
@@ -24,3 +26,21 @@ int maximumNonAdjacentSum(vector<int> &nums){
 
 // TC = O(n)
 // SC = O(n) + O(n)
+
+
+// Solution for better understanding
+int solve(int index, int n, vector<int> &nums, vector<int> &dp){
+        if(index >= n){
+            return 0;
+        }
+        if(dp[index] != -1){
+            return dp[index];
+        }
+        // if(index == n-1) condn is handled in below line.
+        int rob = nums[index] + solve(index + 2, n, nums, dp);
+        int notrob = solve(index+1, n, nums, dp);
+        return dp[index] = max(rob, notrob);
+}
+
+
+
